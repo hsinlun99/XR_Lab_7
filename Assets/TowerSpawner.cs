c357c920baad729abb5ade2ef99a6ea9d634cdc7
hsinlun99 <hsinlun.chiu@gmail.com>
2025-02-27 22:37:55 +0100

    part 4 finished

--- Assets/TowerSpawner.cs
+++ Assets/TowerSpawner.cs
@@ -6,10 +6,12 @@ public class TowerSpawner : NetworkBehaviour
     [SerializeField] private GameObject jengaBlockPrefab;
     [SerializeField] private Vector3 startingPosition = new Vector3(0, 1.05f, 0);
 
+    [SerializeField] private int numberOfLayers = 6;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,13 +22,83 @@ public class TowerSpawner : NetworkBehaviour
 
     public override void OnNetworkSpawn()
     {
-        if (!IsServer) 
+        if (!IsServer)
         {
             return;
         }
 
-        GameObject newBlock = Instantiate(jengaBlockPrefab, startingPosition, Quaternion.identity);
-        NetworkObject nObj = newBlock.GetComponent<NetworkObject>();
-        nObj.Spawn();
+        for (int layer = 0; layer < numberOfLayers; layer++)
+
+        {
+
+            //Check if we are on an odd layer
+
+            bool isOddLayer = layer % 2 == 1;
+
+            //Calculate the Y position for the new Jenga blocks on this layer 
+
+            //based on the starting position, the height of the block and the layer number
+
+            float newBlockHeight = startingPosition.y + layer * jengaBlockPrefab.gameObject.transform.localScale.y;
+
+            // On this layer, Loop over 3 times and Spawn 3 blocks
+
+            // Center them so that the middle block is at 0
+
+            // and the left/right blocks are at ±blockWidth.
+
+            for (int i = 0; i < 3; i++)
+
+            {
+
+                Vector3 newBlockPosition;
+
+                Quaternion newBlockRotation;
+
+                // Position and rotate blocks based on if odd or even layer 
+
+                //If even layer is oriented along the X axis, use Z offset
+
+                if (!isOddLayer)
+
+                {
+
+                    float zOffset = (1 - i) * jengaBlockPrefab.gameObject.transform.localScale.z;
+
+                    newBlockPosition = new Vector3(startingPosition.x, newBlockHeight, startingPosition.z + zOffset);
+
+                    newBlockRotation = Quaternion.Euler(0, 0, 0);
+
+                }
+
+                //else if odd layer and riented along the Z axis, use X offset
+
+                else
+
+                {
+
+                    float xOffset = (1 - i) * jengaBlockPrefab.gameObject.transform.localScale.z;
+
+                    newBlockPosition = new Vector3(startingPosition.x + xOffset, newBlockHeight, startingPosition.z);
+
+                    newBlockRotation = Quaternion.Euler(0, 90, 0);
+
+                }
+
+                // Instantiate new Jenga block using position and rotation 
+
+                //calculated above
+
+                GameObject newBlock = Instantiate(jengaBlockPrefab, newBlockPosition, newBlockRotation);
+
+                //Grab new Jenga block's NetworkObject and spawn it
+
+                NetworkObject nObj = newBlock.GetComponent<NetworkObject>();
+
+                nObj.Spawn();
+
+            }
+
+        }
     }
 }
